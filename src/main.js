2db77b3d2d13f238a66d283eb8209981b5d9e76a
tr-shishir <trshishir09@gmail.com>
2022-11-26 20:00:10 +0600

    check location url

--- src/main.js
+++ src/main.js
@@ -3,3 +3,10 @@ import App from './App.vue';
 import './index.css';
 
 createApp(App).mount('#app');
+
+
+if( urlParams.has('location') ){
+    locationFilter = urlParams.get('location');
+    console.log(locationFilter);
+}
+
