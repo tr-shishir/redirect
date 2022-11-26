11a92e24b0ef7b520b403b68f43458be984b51c3
tr-shishir <trshishir09@gmail.com>
2022-11-26 20:16:46 +0600

    use

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
 
-createApp(App).mount('#app');
-
 let queryString = window.location.search;
 let urlParams = new URLSearchParams(queryString);
 if( urlParams.has('location') ){
     locationFilter = urlParams.get('location');
     console.log(locationFilter);
 }
+
+createApp(App).mount('#app');
+
+
 
