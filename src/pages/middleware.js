3591ef96d2f3de3f97236835260a35e26a69b85c
tr-shishir <trshishir09@gmail.com>
2022-11-27 12:28:57 +0600

    f

--- src/pages/middleware.js
+++ src/pages/middleware.js
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server';
 export async function middleware(req) {
-    const { pathname } = req.nextUrl;
-    if (pathname == '/') {
-        return NextResponse.redirect('/hello-nextjs');
-    }
-    return NextResponse.next();
+  const { pathname } = req.nextUrl;
+  if (pathname == '/') {
+    return NextResponse.redirect('/hello-nextjs');
+  }
+  return NextResponse.next();
 }
